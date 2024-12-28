cd52a60ae9247b38727738e5b45abe9c2343335b
Manjeet Roy <ymanjeet7063@gmail.com>
2024-12-28 18:17:45 +0530

    cards

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -6,16 +6,20 @@ import Card from "@/components/Card/index";
 
 export default function Clubs() {
     return (
-        <>
+        <div className=" bg-white">
             <Header />
             <Banner />
-            <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            <hr />
+            <div className="flex flex-col items-center">
+                <h1 className="text-black my-4 mb-4 text-4xl">List of clubs</h1>
+                <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
             </div>
-        </>
+        </div>
     );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,9 @@
 export default function Card() {
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="bg-white max-w-sm rounded overflow-hidden shadow-lg">
             {/* <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">CardName</div>
+                <div className="text-black font-bold text-xl mb-2">CardName</div>
                 <p className="text-gray-700 text-base">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p>
